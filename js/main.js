85bfc743bdeb56059c71ed688ad4cea24215be84
Julija Markova <julia.markova.ru@gmail.com>
2022-10-11 12:28:01 +0200

    Исправила функцию с длиной комментария

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // Вообще не поняла как нужно было догадаться какая операция выдаст мне рандомное число. Поэтому просто скопировала все из MDN. Не понимаю как это работает
 
-let getRandomInt = function (min, max) { // Задала имя функции, записала ее в переменную, минимальное и максимальное значение - это параметры
+const getRandomInt = function (min, max) { // Задала имя функции, записала ее в переменную, минимальное и максимальное значение - это параметры
   min = Math.ceil(min);
   max = Math.floor(max);
   Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются в расчет
@@ -13,14 +13,28 @@ getRandomInt (1, 158);
 // Проверка максимальной длины строки
 
 
-let string = 'Хочешь еще этих мягких булочек';
+const line = 'Хочешь еще этих мягких булочек';
 
-let checkCommentLength = function (minLength, maxLength) {
-  console.log(string.length);
+const checkCommentLength = function (minLength, maxLength) {
 
-  if (string.length > minLength && string.length < maxLength) {
-    console.log('да');
+  if (line.length > minLength && line.length < maxLength) {
+
+    const number = line.length;
+    // console.log ('Спасибо за ваше мнение');
+    return number;
   }
+
+  // if (line.length < minLength {
+
+  //   const number = line.length;
+  //   console.log ('Комментарий слишком короткий');
+  // }
+
+  // if (line.length > maxLength {
+
+  //   const number = line.length;
+  //   console.log ('Комментарий слишком длинный');
+  // }
 };
 
 checkCommentLength (20, 140);
