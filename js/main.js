30d48e1c2f6d9ce2b356056584dd82b2c1808583
Julija Markova <julia.markova.ru@gmail.com>
2022-10-12 17:32:17 +0200

    Исправила метод округления в функции с рандомным числом

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 function getRandomInt (min, max) {
   if (max <= min || min < 0 || max < 0 || typeof min !== 'number' || typeof max !== 'number') {
     return NaN;
-  } else {return Math.floor(Math.random() * ((max - min + 1)) + min);}
+  } else {return Math.round(Math.random() * ((max - min + 1)) + min);}
 }
 
 getRandomInt (1, 140);
