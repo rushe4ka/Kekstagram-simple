91b76281b50350a636358fae5b14c4617fe169e2
Julija Markova <julia.markova.ru@gmail.com>
2022-10-31 20:45:35 +0100

    Создает миниатюры пользовательских картинок

---
+++ js/create-miniatures.js
@@ -0,0 +1,22 @@
+import {getPictureDescriptions} from 'get-picture-descriptions.js';
+
+const pictures = document.querySelector('.pictures'); // поле для картинок
+const templatePicture = document.querySelector('#picture')
+  .content
+  .querySelector('.picture'); // элемент с образцом разметки
+
+const usersPictures = getPictureDescriptions(25);
+
+const usersPicturesFragment = document.createDocumentFragment();
+
+usersPictures.forEach(({url, comments, likes}) => {
+  const pictureElement = templatePicture.cloneNode(true); // клонирую образец разметки
+  pictureElement.querySelector('.picture__img').img.src = url;
+  pictureElement.querySelector('.picture__comments').textContent = comments;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  usersPicturesFragment.appendChild(pictureElement); // добавляю картинку в фрагмент
+});
+
+pictures.appendChild(usersPicturesFragment);
+
+
