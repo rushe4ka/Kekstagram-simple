5e07415b9d06c6bb1663e55b43ba6b0844f47f0c
Julija Markova <julia.markova.ru@gmail.com>
2022-11-05 13:24:40 +0100

    Переписала модуль добавления эффекта на фото

--- js/overlay-effect.js
+++ js/overlay-effect.js
@@ -1,55 +1,12 @@
 const effectsList = document.querySelector('.effects__list');
-// const effects = Array.from(document.querySelectorAll('[name=effect]'));
 const imgUploadPreview = document.querySelector('.img-upload__preview');
-
-// const EFFECTS = [
-//   'none',
-//   'chrome',
-//   'sepia',
-//   'marvin',
-//   'phobos',
-//   'heat',
-// ];
-
 
 // отслеживать клики по любому пункту списка
 
-function onEffectChange (evt) {
-  if (evt.target.value === 'none') {
-    imgUploadPreview.classList.add(`effects__preview--${evt.target.value}`);
-  } else {
-    imgUploadPreview.classList.remove(`effects__preview--${evt.target.value}`);
-  }
+effectsList.addEventListener ('focusin', (evt) => {
+  imgUploadPreview.classList.add(`effects__preview--${evt.target.value}`);
+});
 
-  if (evt.target.value === 'chrome') {
-    imgUploadPreview.classList.add(`effects__preview--${evt.target.value}`);
-  } else {
-    imgUploadPreview.classList.remove(`effects__preview--${evt.target.value}`);
-  }
-
-  if (evt.target.value === 'sepia') {
-    imgUploadPreview.classList.add('effects__preview--sepia');
-  } else {
-    imgUploadPreview.classList.remove('effects__preview--sepia');
-  }
-
-  if (evt.target.value === 'marvin') {
-    imgUploadPreview.classList.add('effects__preview--marvin');
-  } else {
-    imgUploadPreview.classList.remove('effects__preview--marvin');
-  }
-
-  if (evt.target.value === 'phobos') {
-    imgUploadPreview.classList.add('effects__preview--phobos');
-  } else {
-    imgUploadPreview.classList.remove('effects__preview--phobos');
-  }
-
-  if (evt.target.value === 'heat') {
-    imgUploadPreview.classList.add('effects__preview--heat');
-  } else {
-    imgUploadPreview.classList.remove('effects__preview--heat');
-  }
-}
-
-effectsList.addEventListener('change', onEffectChange);
+effectsList.addEventListener ('focusout', (evt) => {
+  imgUploadPreview.classList.remove(`effects__preview--${evt.target.value}`);
+});
