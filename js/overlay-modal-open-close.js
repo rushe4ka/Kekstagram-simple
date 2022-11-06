26138720b87a98ceeb255f74b4492ea4758f1497
Julija Markova <julia.markova.ru@gmail.com>
2022-11-06 15:29:27 +0100

    Добавила удаление подписки на клики по крестику при закрытии окна редактирования

--- js/overlay-modal-open-close.js
+++ js/overlay-modal-open-close.js
@@ -7,32 +7,9 @@ const uploadOverlay = body.querySelector('.img-upload__overlay');
 const uploadOverlayCancel = uploadOverlay.querySelector('#upload-cancel');
 const imgUploadPreview = document.querySelector('.img-upload__preview img');
 
-const onModalEscKeydown = (evt) => {
-  if (isEscapeKey(evt)) {
-    evt.preventDefault();
-    closeOverlayModal();
-  }
-};
-
-function openOverlayModal() {
-  uploadOverlay.classList.remove('hidden');
-  body.classList.add('modal-open');
-  document.addEventListener('keydown', onModalEscKeydown);
-}
-
-function closeOverlayModal() {
-  uploadOverlay.classList.add('hidden');
-  body.classList.remove('modal-open');
-  document.removeEventListener('keydown', onModalEscKeydown);
-  uploadForm.reset();
-  imgUploadPreview.removeAttribute('class');
-}
-
 // при изменении поля загрузки открыть окно редактирования
 
-uploadInput.addEventListener('change', () => {
-  openOverlayModal();
-});
+uploadInput.addEventListener('change', (openOverlayModal));
 
 // при фокусе с клавиатуры открыть окно редактирования
 
@@ -42,17 +19,34 @@ uploadInput.addEventListener('keydown', (evt) => {
   }
 });
 
-// при клике на крестик закрыть окно редактирования
-
-uploadOverlayCancel.addEventListener('click', () => {
-  closeOverlayModal();
-});
-
 // при нажатии Enter закрыть окно редактирования
 
-uploadOverlayCancel.addEventListener('keydown', (evt) => {
-  if (isEnterKey(evt)) {
+const onModalEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
     evt.preventDefault();
     closeOverlayModal();
   }
-});
+};
+
+// при клике на крестик закрыть окно редактирования
+
+const onModalEscButtonClick = uploadOverlayCancel.addEventListener('click', (closeOverlayModal));
+
+// открытие окна редактирования изображения
+
+function openOverlayModal() {
+  uploadOverlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onModalEscKeydown);
+  document.removeEventListener('click', onModalEscButtonClick);
+}
+
+// закрытие окна редактирования изображения
+
+function closeOverlayModal() {
+  uploadOverlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onModalEscKeydown);
+  uploadForm.reset();
+  imgUploadPreview.removeAttribute('class');
+}
