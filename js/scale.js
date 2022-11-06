73d8b37701c4c641f14075eada88e29ebe51cc37
Julija Markova <julia.markova.ru@gmail.com>
2022-11-06 15:29:48 +0100

    Исправила написание переменных

--- js/scale.js
+++ js/scale.js
@@ -6,28 +6,32 @@ const scaleControlSmaller = imgUploadScale.querySelector('.scale__control--small
 const scaleControlBigger = imgUploadScale.querySelector('.scale__control--bigger');
 const scaleControlValue = imgUploadScale.querySelector('.scale__control--value');
 const imgUploadPreview = document.querySelector('.img-upload__preview');
-const maxScale = 100;
-const minScale = 25;
-const step = 25;
+const MAX_SCALE = 100;
+const MIN_SCALE = 25;
+const STEP = 25;
+
+// кнопка "меньше"
 
 scaleControlSmaller.addEventListener('click', () => {
   const currentValue = scaleControlValue.value;
   const numberValue = toNumber(removeLastChar(currentValue));
 
-  if (numberValue > minScale && numberValue <= maxScale) {
-    const newValue = numberValue - step;
+  if (numberValue > MIN_SCALE && numberValue <= MAX_SCALE) {
+    const newValue = numberValue - STEP;
 
     scaleControlValue.value = `${newValue}%`;
     imgUploadPreview.style.transform = `scale(${newValue / 100})`;
   }
 });
 
+// кнопка "больше"
+
 scaleControlBigger.addEventListener('click', () => {
   const currentValue = scaleControlValue.value;
   const numberValue = toNumber(removeLastChar(currentValue));
 
-  if (numberValue >= minScale && numberValue < maxScale) {
-    const newValue = numberValue + step;
+  if (numberValue >= MIN_SCALE && numberValue < MAX_SCALE) {
+    const newValue = numberValue + STEP;
 
     scaleControlValue.value = `${newValue}%`;
     imgUploadPreview.style.transform = `scale(${newValue / 100})`;
