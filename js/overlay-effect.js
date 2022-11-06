b175d7d4bef4ac5f836babe6b92329e937a80137
Julija Markova <julia.markova.ru@gmail.com>
2022-11-06 15:06:43 +0100

    Исправила функцию добавления эффекта на картинку

--- js/overlay-effect.js
+++ js/overlay-effect.js
@@ -1,12 +1,8 @@
 const effectsList = document.querySelector('.effects__list');
-const imgUploadPreview = document.querySelector('.img-upload__preview');
+const imgUploadPreview = document.querySelector('.img-upload__preview img');
 
 // отслеживать клики по любому пункту списка
 
-effectsList.addEventListener ('focusin', (evt) => {
-  imgUploadPreview.classList.add(`effects__preview--${evt.target.value}`);
-});
-
-effectsList.addEventListener ('focusout', (evt) => {
-  imgUploadPreview.classList.remove(`effects__preview--${evt.target.value}`);
+effectsList.addEventListener ('click', (evt) => {
+  imgUploadPreview.className = (`effects__preview--${evt.target.value}`);
 });
