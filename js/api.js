9b7d599524cbf4045ecd098cbcd2a632982a3038
Julija Markova <julia.markova.ru@gmail.com>
2022-11-13 18:46:03 +0100

    Пишу функцию на предупреждение о неуспешной загрузке данных с сервера

--- js/api.js
+++ js/api.js
@@ -1,12 +1,40 @@
+// окно предупреждения об ошибке получения данных с сервера
+
+const ALERT_SHOW_TIME = 5000;
+
+const showAlert = (message) => {
+  const alert = document.createElement('div');
+  alert.style.zIndex = '100';
+  alert.style.position = 'absolute';
+  alert.style.left = '0';
+  alert.style.top = '0';
+  alert.style.right = '0';
+  alert.style.padding = '10px 3px';
+  alert.style.fontSize = '30px';
+  alert.style.textAlign = 'center';
+  alert.style.backgroundColor = 'red';
+
+  alert.textContent = message;
+
+  document.body.append(alert);
+
+  setTimeout(() => {
+    alert.remove();
+  }, ALERT_SHOW_TIME);
+};
+
 // получаем список картинок с сервера
 
-const getData = (onSuccess) => {
+const getData = (onSuccess, onFail) => {
   fetch(
     'https://27.javascript.pages.academy/kekstagram-simple/data'
   )
     .then((response) => response.json())
     .then((data) => {
       onSuccess(data);
+    })
+    .catch(() => {
+      onFail(showAlert);
     });
 };
 
--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 import './overlay-effect.js';
 import './scale.js';
 
-import { closeOverlayModal } from './overlay-modal.js';
+import { closeOverlayModal } from './modals.js';
 import { setUserFormSubmit } from './form.js';
 import { getData } from './api.js';
 import { renderPicturesList } from './create-miniatures.js';
 
-// закрываем окно редактирования при отправке формы
-
 getData ((data) => {
   renderPicturesList(data);
 });
+
+// закрываем окно редактирования при отправке формы
 
 setUserFormSubmit(closeOverlayModal);
