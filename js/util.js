85269609258c549a1cd77e6b81ace3a2cc899a00
Julija Markova <julia.markova.ru@gmail.com>
2022-11-13 18:46:21 +0100

    Убрала лишний код

--- js/util.js
+++ js/util.js
@@ -53,39 +53,11 @@ const removeLastChar = (string) => string ? string.slice(0,-1) : string;
 
 // ============================================ //
 
-// окно предупреждения об ошибке отправки формы
-
-const ALERT_SHOW_TIME = 5000;
-
-const showAlert = (message) => {
-  const alertContainer = document.createElement('div');
-  alertContainer.style.zIndex = '100';
-  alertContainer.style.position = 'absolute';
-  alertContainer.style.left = '0';
-  alertContainer.style.top = '0';
-  alertContainer.style.right = '0';
-  alertContainer.style.padding = '10px 3px';
-  alertContainer.style.fontSize = '30px';
-  alertContainer.style.textAlign = 'center';
-  alertContainer.style.backgroundColor = 'red';
-
-  alertContainer.textContent = message;
-
-  document.body.append(alertContainer);
-
-  setTimeout(() => {
-    alertContainer.remove();
-  }, ALERT_SHOW_TIME);
-};
-
-// ============================================ //
-
 export {
   // getRandomPositiveInteger,
   // getRandomArrayElement,
   isEscapeKey,
   isEnterKey,
   toNumber,
-  removeLastChar,
-  showAlert
+  removeLastChar
 };
