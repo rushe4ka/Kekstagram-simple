8e2988d9c037bbc1c3205b13177fa29d075aea6f
Julija Markova <julia.markova.ru@gmail.com>
2022-11-14 20:25:52 +0100

    Убрала текст alert сообщения в переменную

--- js/api.js
+++ js/api.js
@@ -1,27 +1,6 @@
 // окно предупреждения об ошибке получения данных с сервера
 
-const ALERT_SHOW_TIME = 5000;
-
-const showAlert = (message) => {
-  const alert = document.createElement('div');
-  alert.style.zIndex = '100';
-  alert.style.position = 'absolute';
-  alert.style.left = '0';
-  alert.style.top = '0';
-  alert.style.right = '0';
-  alert.style.padding = '10px 3px';
-  alert.style.fontSize = '30px';
-  alert.style.textAlign = 'center';
-  alert.style.backgroundColor = 'red';
-
-  alert.textContent = message;
-
-  document.body.append(alert);
-
-  setTimeout(() => {
-    alert.remove();
-  }, ALERT_SHOW_TIME);
-};
+const alertMessage = 'Не удалось отправить форму. Попробуйте ещё раз';
 
 // получаем список картинок с сервера
 
@@ -52,11 +31,11 @@ const sendData = (onSuccess, onFail, body) => {
       if (response.ok) {
         onSuccess();
       } else {
-        onFail('Не удалось отправить форму. Попробуйте ещё раз');
+        onFail(alertMessage);
       }
     })
     .catch(() => {
-      onFail('Не удалось отправить форму. Попробуйте ещё раз');
+      onFail(alertMessage);
     });
 };
 
