c196532aaec5138aa7243b9c2460652aa15a4882
Julija Markova <julia.markova.ru@gmail.com>
2022-11-15 23:45:44 +0100

    Спрятала слайдер

--- js/main.js
+++ js/main.js
@@ -6,7 +6,13 @@ import { setUserFormSubmit } from './form.js';
 import { getData } from './api.js';
 import { renderPicturesList } from './create-miniatures.js';
 
+const slider = document.querySelector('.effect-level');
+const hideSlider = () => {
+  slider.classList.add('visually-hidden');
+};
+
 getData ((data) => {
   renderPicturesList(data);
   setUserFormSubmit(closeOverlayModal); // создание обработчика отправки формы
+  hideSlider();
 });
