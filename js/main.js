a7594de92b0630c4b6d2461d834bc0dd8f12d837
Julija Markova <julia.markova.ru@gmail.com>
2022-11-19 22:12:25 +0100

    Спрятала слайдер через разметку, функцию убрала

--- js/main.js
+++ js/main.js
@@ -5,13 +5,9 @@ import { setUserFormSubmit, closeOverlayModal, showErrorMessage } from './form.j
 import { getData } from './api.js';
 import { renderPicturesList } from './create-miniatures.js';
 
-const slider = document.querySelector('.effect-level');
-const hideSlider = () => slider.classList.add('visually-hidden');
-
 getData ((data) => {
   renderPicturesList(data);
   setUserFormSubmit(closeOverlayModal); // создание обработчика отправки формы
-  hideSlider();
 }, (text) => {
   showErrorMessage(text);
 });
